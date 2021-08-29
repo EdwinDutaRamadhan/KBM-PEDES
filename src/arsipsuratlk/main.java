daf5e7d3abe0c0299aeb1edd044bdcf103c2d296
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2021-08-29 20:35:52 +0700

    ARSIP SURAT LK

---
+++ src/arsipsuratlk/main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arsipsuratlk;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author @edwindtr__
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        pageLogin ed=new pageLogin();
+        ed.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        ed.setVisible(true);
+    }
+    
+}
